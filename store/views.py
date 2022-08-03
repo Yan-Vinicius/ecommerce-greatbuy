93d7e4e0225c77965fdf02e2473d41b62ca32e1a
Yan Vinicius <yan.vinicius.queiroz@gmail.com>
2022-08-03 14:17:37 -0300

    Adição da função de calcular a quantidade de produtos no carrinho

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from carts.models import CartItem
+from carts.views import _cart_id
 from category.models import Category
 from store.models import Product
 
@@ -29,10 +31,11 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=product).exists()
 
     except Exception as e:
         raise e
 
-    context = {'product': product}
+    context = {'product': product, 'in_cart': in_cart}
 
     return render(request, 'store/product_detail.html', context)
