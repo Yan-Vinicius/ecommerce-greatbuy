91b323e024c40cd1ccf0e035f3eae135d29adc10
Yan Vinicius <yan.vinicius.queiroz@gmail.com>
2022-08-09 11:31:50 -0300

    Adição e/ou modificação das funções de carrinho e da loja

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
-from store.models import Product
+from store.models import Product, Variation
 
 
 # Create your views here.
@@ -18,6 +19,21 @@ def _cart_id(request):
 def add_to_cart(request, product_id):
 
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        #esse for serve para checar todas as infos que virão no POST
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key,
+                                                  variation_value__iexact=value)
+                product_variation.append(variation)
+
+            except:
+                pass
 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -27,36 +43,74 @@ def add_to_cart(request, product_id):
 
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
-        cart_item.save()
+    cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
 
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
-        cart_item.save()
+    if cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
 
-    return redirect('cart')
+        # variações existentes -> banco de dados
+        # variação atual -> product_variation
+        # item_id -> banco de dados
+        existing_var_list = []
+        list_id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            existing_var_list.append(list(existing_variation))
+            list_id.append(item.id)
 
+        if product_variation in existing_var_list:
+            index = existing_var_list.index(product_variation)
+            item_id = list_id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
 
-def remove_cart(request, product_id):
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+        else:
+            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
 
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+
+            item.save()
+
     else:
-        cart_item.delete()
+
+        cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
+
+        cart_item.save()
 
     return redirect('cart')
 
 
-def remove_cart_item(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
+
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+
+    try:
+
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+
+    except:
+        pass
+
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id, cart_item_id):
+
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
 
     cart_item.delete()
 
@@ -72,7 +126,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
 
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {'total': total, 'quantity': quantity, 'cart_items': cart_items}
--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -5,25 +7,32 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
 from store.models import Product
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def store(request, category_slug=None):
 
     categories = None
     products = None
-
+    category = Category.objects.all()
     if category_slug is not None:
 
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
 
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 4)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
-    context = {'products': products, 'product_count': product_count}
+    context = {'products': paged_products, 'product_count': product_count, 'categories': category}
 
     return render(request, 'store/store.html', context)
 
@@ -39,3 +48,19 @@ def product_detail(request, category_slug, product_slug):
     context = {'product': product, 'in_cart': in_cart}
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            # Q serve para selecionar mais de um campo para fazer pesquisa
+            # icontains serve para ver se a keyword está no campo que eu quero
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) |
+                                                                        Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+        context = {'products': products, 'product_count': product_count}
+
+    return render(request, 'store/store.html', context)
