12623efb3768a46b800a2c10a50b9398428bc28e
Yan Vinicius <yan.vinicius.queiroz@gmail.com>
2022-08-14 20:14:44 -0300

    Adição da função de alerta na hora de registrar

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
 
 
@@ -21,9 +22,11 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email,
                                                username=username, password=password)
             user.phone_number = phone_number
-
             user.save()
 
+            messages.success(request, 'Registration was successful')
+
+            return redirect('register')
     else:
         form = RegistrationForm()
 
